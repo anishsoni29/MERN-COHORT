6a959762e9a9118a5ce61ebed094daeb04d49d99
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-16 00:47:10 +0530

    added the complete code for the render application

--- Day-22/code/react-components/src/App.jsx
+++ Day-22/code/react-components/src/App.jsx
@@ -6,24 +6,39 @@ import React, { useState } from "react";
 
 function App() {
   //state variable
+
+  return (
+    <div>
+      <HeaderWithButton />
+      <Header title="Anish 1" />
+      <Header title="Anish 2" />
+    </div>
+  );
+}
+
+//we have made a different component here such that only the thing which was meant to re-render does, not the entire children of the parent body.
+//push the state down to the button in the tree --> the parent will not re-render (put it to the lowest leave)
+//.. or you can use react.memo()
+
+function HeaderWithButton() {
+  //define the state variables also here
   const [title, setTitle] = useState("my name is Anish");
 
   function updateTitle() {
     setTitle("My name is " + Math.random());
   }
-
   return (
     <div>
       <button onClick={updateTitle}> Update the title </button>
       <Header title={title} />
-      <Header title="Anish" />
     </div>
   );
 }
 
 //object destructuring --> no need to use props
-function Header({ title }) {
+
+const Header = React.memo(function Header({ title }) {
   return <div>{title}</div>;
-}
+});
 
 export default App;
