f434bce826fae1d3fbd14c3cbea8df93f55f46e0
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-16 01:29:20 +0530

    added the code for the todo

--- Day-22/code/react-components/src/App.jsx
+++ Day-22/code/react-components/src/App.jsx
@@ -1,44 +1,58 @@
-//re-render assignment
-
 import React, { useState } from "react";
 
-//react-returns
-
 function App() {
-  //state variable
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Go to the gym",
+      description: "You need to go to the gym",
+    },
+    {
+      id: 2,
+      title: "Go to market",
+      description: "You need to go with Mummy",
+    },
+    {
+      id: 3,
+      title: "Read that book",
+      description: "Read the Cave by Alok Kejriwal!",
+    },
+  ]);
 
-  return (
-    <div>
-      <HeaderWithButton />
-      <Header title="Anish 1" />
-      <Header title="Anish 2" />
-    </div>
-  );
-}
-
-//we have made a different component here such that only the thing which was meant to re-render does, not the entire children of the parent body.
-//push the state down to the button in the tree --> the parent will not re-render (put it to the lowest leave)
-//.. or you can use react.memo()
-
-function HeaderWithButton() {
-  //define the state variables also here
-  const [title, setTitle] = useState("my name is Anish");
-
-  function updateTitle() {
-    setTitle("My name is " + Math.random());
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: 4,
+        title: Math.random().toString(),
+        description: Math.random().toString(),
+      },
+    ]);
   }
+
   return (
     <div>
-      <button onClick={updateTitle}> Update the title </button>
-      <Header title={title} />
+      <button onClick={addTodo}>Add a todo</button>
+      {todos.map(function (todo) {
+        return (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+          />
+        );
+      })}
     </div>
   );
 }
 
-//object destructuring --> no need to use props
-
-const Header = React.memo(function Header({ title }) {
-  return <div>{title}</div>;
-});
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h4>{description}</h4>
+    </div>
+  );
+}
 
 export default App;
