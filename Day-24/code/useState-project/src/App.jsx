7e5a41ea7c1b0a0aa0fedf527a36a32077e516d9
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-16 22:50:07 +0530

    deleted the extra node packages installed at the home dir

--- Day-24/code/useState-project/src/App.jsx
+++ Day-24/code/useState-project/src/App.jsx
@@ -1,30 +1,35 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
   return (
     <div>
-      <Todo id={1} />
+      <Todo id={1}></Todo>
     </div>
   );
+}
 
-  //initialising the state variables
-  function Todo() {
-    const [todo, setTodo] = useState({});
-  }
-  //useEffect hook to fetch data from the api and non infinitely.
+function Todo() {
+  const [todo, setTodo] = useState({});
+
+  //implement effect here -->
+
   useEffect(() => {
     axios
-      .get("https://sum-server.100xdevs.com/todo?id=1")
-      .then(function (response) {
-        setTodos(response.data.todos);
+      .get("https://sum-server.100xdevs.com/todo?id=1" + id)
+      .then((response) => {
+        setTodo(response.data.todo);
       });
-  }, []);
+  }),
+    [];
+
+  //the data.todo is being fetched from the backend and it is the boilerplate of the axios library.
+
   return (
-    <>
+    <div>
       <h1>{todo.title}</h1>
       <h2>{todo.description}</h2>
-    </>
+    </div>
   );
 }
 
