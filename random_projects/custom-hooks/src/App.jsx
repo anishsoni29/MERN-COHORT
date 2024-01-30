46eb8b3897ccf63a3dc91e1efb53bdedcd8b9b78
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-30 23:40:44 +0530

    added random_projects

---
+++ random_projects/custom-hooks/src/App.jsx
@@ -0,0 +1,24 @@
+import { useDebugValue, useEffect } from "react";
+
+function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+}
+
+//using custom hooks
+function MyComponent() {
+  useEffect(() => {
+    console.error("MyComponent is mounted");
+
+    return () => {
+      console.log("MyComponent is unmounted");
+    };
+  }, []); //--> this [] is the dependency array, anytime this changes, the code reruns.
+
+  return <div>From inside MyComponent</div>;
+}
+
+export default App;
