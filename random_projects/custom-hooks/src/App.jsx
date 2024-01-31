f56351ec91fe402e54429c3c49253f852b8f12f6
Anish Soni <anish.21cse189@rtu.ac.in>
2024-01-31 11:55:23 +0530

    code for custom hooks initiated

--- random_projects/custom-hooks/src/App.jsx
+++ random_projects/custom-hooks/src/App.jsx
@@ -1,36 +1,34 @@
-//explaination of React defined hooks
-import { useDebugValue, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { axios } from "./axios";
 
-//defining the state variablles here
-const [render, setRender] = useState(true);
+//designing the custom hook : A hook is basically a function which starts with use and has a self calling hook either React based or custom component.
 
 function App() {
-  //to make the MyComponent function to stop running after 5 seconds,
-  // we can use the useEffect hook with the setTimeout function
-  useEffect(() => {
-    setInterval(() => {
-      setRender((r) => !r); //this makes it to stop re-rendering after the setTimeout interval.
-    }, 5000);
-  }, []); //--> using the dependency array to make it run according to the changes made.
+  const [todos, setTodos] = useState([]);
 
-  //returning if the render component is true, if not then return only the div component
-  return <>{render ? <MyComponent /> : <div></div>}</>;
-  //this is a ternary operator, if the render is true, then return the MyComponent, if not then return an empty div
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos").then((res) => {
+      setTodos(res.data.todos);
+    });
+  }, []);
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <Track todo={todo}></Track>
+      ))}
+    </div>
+  );
 }
 
-//using custom hooks
-function MyComponent() {
-  useEffect(() => {
-    console.error("MyComponent is mounted");
-    //the code goes to the bottom of the page when the dependency array changes and then it goes to the top
-
-    return () => {
-      console.log("MyComponent is unmounted");
-    };
-  }, []); //--> this [] is the dependency array, anytime this changes, the code reruns.
-  //changing the value of the dependency array will cause the code to rerun.
-
-  return <div>From inside MyComponent</div>;
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
 }
 
 export default App;
