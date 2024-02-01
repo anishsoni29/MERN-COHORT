d144722dc02189cba52e325ffdd25bdaea8bd88d
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-02 00:32:23 +0530

    added the code to convert the typescript command into js command

---
+++ node-app/a.js
@@ -0,0 +1,8 @@
+"use strict";
+//this is my first typescrpt file
+//so here whenever you are defining a variable, you need to give it a type : --> here the type is number.
+
+//the command tsc and tsc -b has converted the typescript code into javascript command.
+
+const x = 1;
+console.log(x);
