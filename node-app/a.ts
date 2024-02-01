edcd968e14adf835bb7a2fecdcb66c38699d2114
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-02 00:53:07 +0530

    returned how to give the type to the function

--- node-app/a.ts
+++ node-app/a.ts
@@ -1,7 +1,31 @@
-function greet(firstName: string) {
-  console.log("Hello" + firstName);
+function sum(a: number, b: number) {
+  return a + b;
 }
 
-greet("Anish");
+//type inference:
 
-//ts gives you typesafety
+const value = sum(1, 2);
+console.log(value);
+
+//you don't need to explicitly define the return type.
+//the hovering is the function signature, which already defines it.
+
+function isLegal(age: number) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+isLegal(19);
+
+//A function which takes another function as an argument.
+
+//the type here is void.
+function runAfter1S(fn) {
+  setTimeout(fn, 1000);
+}
+
+runAfter1S(() => {
+  console.log("Hi there, this is the second function!");
+});
