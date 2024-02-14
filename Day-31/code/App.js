93892bed23d7846d3de1faecdad97d597592266e
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-14 23:43:46 +0530

    edited the incorrect code

--- Day-31/code/App.js
+++ Day-31/code/App.js
@@ -1,6 +1,6 @@
-function getFirstElement<T>(arg:T[]{
-    return arg[0];
-})
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
 //array of the template [T] is the input and the return value is arr[0]
 
 const el = getFirstElement(["anishSoni", "PiyushSoni"]);
