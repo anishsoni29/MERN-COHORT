d77aa3f85a09177587627016da3bb1da899c7f88
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-19 11:20:00 +0530

    getting the input from the users

--- serverless-fns/hono-app/src/index.ts
+++ serverless-fns/hono-app/src/index.ts
@@ -2,8 +2,12 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  //body, headers, query-parameteres, etc
+app.get("/", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
   return c.text("Hello Hono!");
 });
 
