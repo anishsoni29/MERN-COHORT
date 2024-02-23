6fe92f885c8dd64eb337c957c57cbbf27db9d68e
Anish Soni <anish.21cse189@rtu.ac.in>
2024-02-23 23:47:32 +0530

    changed the staging

--- serverless-fns/my-app/src/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Env {}
-
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		return Response.json({
-			message: 'you did not send a get request',
-		});
-	},
-};
