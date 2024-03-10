48601cde795d4ea4db1b454f548a6d9e21f88264
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-10 18:22:38 +0530

    added the connection pool and database url

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+//adding the routes
+//c is the context object which has the req,res and next properties
+app.post("/api/vi/signup", (c) => {
+  return c.text("Signup Route");
+});
 
-export default app
+app.post("/api/vi/login", (c) => {
+  return c.text("Signin Route");
+});
+
+app.post("/api/vi/blog", (c) => {
+  return c.text("Blog Route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Update Blog Route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Get Blog Route");
+});
+
+export default app;
+
+//connection pool url in wrangler.toml
+//database url in .env file
+//you can't use local environment with connection pool hence no docker.
