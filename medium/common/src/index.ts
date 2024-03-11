1d316245f00db6bb75255bba7d2065dbda207c70
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-11 23:26:40 +0530

    added the frontend and backend zod validation code

--- medium/common/src/index.ts
+++ medium/common/src/index.ts
@@ -1,10 +1,32 @@
+//zod validation module ==>
 import z from "zod";
 
+//adding exports and type inferences
+//code for backend
 export const signupInput = z.object({
   username: z.string().email(),
   password: z.string().min(6),
   name: z.string().optional(),
 });
 
-//type inference in  zod
+export const signinInput = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const createBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export const updateBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+  id: z.number(),
+});
+
+//code for frontend
 export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
