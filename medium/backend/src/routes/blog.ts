4caf121e72639b568bf3ef625e53bc9e9208be76
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-11 22:55:19 +0530

    added try and catch

--- medium/backend/src/routes/blog.ts
+++ medium/backend/src/routes/blog.ts
@@ -4,82 +4,93 @@ import { Hono } from "hono";
 import { verify } from "hono/jwt";
 
 export const bookRouter = new Hono<{
-    Bindings: {
-        DATABASE_URL: string;
-        JWT_SECRET: string;
-    },
-    Variables: {
-        userId: string
-    }
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
 }>();
 
 bookRouter.use(async (c, next) => {
-    const jwt = c.req.header('Authorization');
-	if (!jwt) {
-		c.status(401);
-		return c.json({ error: "unauthorized" });
-	}
-	const token = jwt.split(' ')[1];
-	const payload = await verify(token, c.env.JWT_SECRET);
-	if (!payload) {
-		c.status(401);
-		return c.json({ error: "unauthorized" });
-	}
-	c.set('userId', payload.id);
-	await next()
+  try {
+    const jwt = c.req.header("Authorization");
+    if (!jwt) {
+      c.status(401);
+      return c.json({ error: "unauthorized" });
+    }
+    const token = jwt.split(" ")[1];
+    const payload = await verify(token, c.env.JWT_SECRET);
+
+    // If payload is undefined, it indicates an unauthorized access
+    if (!payload) {
+      c.status(401);
+      return c.json({ error: "unauthorized" });
+    }
+
+    // Setting userId in the header
+    c.set("userId", payload.id);
+    await next();
+  } catch (error) {
+    // Handling any errors that occur during token verification or processing
+    console.error("Error occurred:", error);
+    c.status(500);
+    return c.json({ error: "Internal Server Error" });
+  }
 });
 
-bookRouter.post('/', async (c) => {
-	const userId = c.get('userId');
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
+bookRouter.post("/", async (c) => {
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-	const body = await c.req.json();
-	const post = await prisma.post.create({
-		data: {
-			title: body.title,
-			content: body.content,
-			authorId: userId
-		}
-	});
-	return c.json({
-		id: post.id
-	});
-})
-
-bookRouter.put('/', async (c) => {
-	const userId = c.get('userId');
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
-	prisma.post.update({
-		where: {
-			id: body.id,
-			authorId: userId
-		},
-		data: {
-			title: body.title,
-			content: body.content
-		}
-	});
-
-	return c.text('updated post');
+  const body = await c.req.json();
+  const post = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: userId,
+    },
+  });
+  return c.json({
+    id: post.id,
+  });
 });
 
-bookRouter.get('/:id', async (c) => {
-	const id = c.req.param('id');
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate());
-	
-	const post = await prisma.post.findUnique({
-		where: {
-			id
-		}
-	});
+bookRouter.put("/", async (c) => {
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-	return c.json(post);
-})
+  const body = await c.req.json();
+  prisma.post.update({
+    where: {
+      id: body.id,
+      authorId: userId,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+
+  return c.text("updated post");
+});
+
+bookRouter.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const post = await prisma.post.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return c.json(post);
+});
