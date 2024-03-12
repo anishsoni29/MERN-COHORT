ac85c8e13e5f3ea7b125e6c0a61504da203446f0
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-13 00:05:58 +0530

    alligned the text in the middle

--- medium/frontend/src/components/Quote.tsx
+++ medium/frontend/src/components/Quote.tsx
@@ -2,17 +2,18 @@ export const Quote = () => {
   return (
     <div className="bg-slate-200 h-screen flex justify-center flex-col">
       <div className="flex justify-center">
-        {/* comes under the parent component */}
-        <div className="max-w-lg text-3xl font-bold">
-          "The customer support I received was exceptional. The support team
-          went above and beyond to address my concerns."{" "}
+        <div className="max-w-lg">
+          <div className="text-3xl font-bold">
+            "The customer support I received was exceptional. The support team
+            went above and beyond to address my concerns".
+          </div>
+          <div className="pt-5 max-w-md text-xl font-semibold">
+            Julies Winfield
+          </div>
+          <div className="max-w-md font-light text-md text-slate-500">
+            CEO | Acne Max
+          </div>
         </div>
-      </div>
-      <div className="max-w-md text-center text-xl font-semibold">
-        Julies Winfield
-      </div>
-      <div className="max-w-md text-center font-light text-md text-slate-500">
-        CEO | Acne Max
       </div>
     </div>
   );
