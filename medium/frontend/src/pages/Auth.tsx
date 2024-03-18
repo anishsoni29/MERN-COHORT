9094462e74fbf9fefab5eef823eff2932537a1b9
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-18 16:51:49 +0530

    added the account and login component

--- medium/frontend/src/pages/Auth.tsx
+++ medium/frontend/src/pages/Auth.tsx
@@ -1,7 +1,20 @@
-export const Auth = () => {
+import { Link } from "react-router-dom";
+
+//adding the types of signup and signin
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
   return (
     <div className="h-screen flex justify-center flex-col">
-      <div>Signup Auth Component</div>
+      <div className="flex justify-center">
+        <div>
+          <div className="text-3xl font-extrabold">Create an account!</div>
+          <div className="text-slate-400">
+            Already have an account?
+            <Link className="pl-2 underline" to={"/signin"}>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
