fb482630ca4341aa8beebecd7f6f450e9c28ba82
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-22 13:05:48 +0530

    defer the Button as client side component

--- bootstrap-NEXTJS/next-app/components/Signin.tsx
+++ bootstrap-NEXTJS/next-app/components/Signin.tsx
@@ -1,9 +1,6 @@
-"use client";
+import { Button } from "./Button";
 
 export function Signin() {
-  function handler() {
-    console.log("sign in button clicker");
-  }
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -22,13 +19,7 @@ export function Signin() {
                 type={"password"}
                 placeholder="123456"
               />
-              <button
-                onClick={handler}
-                type="button"
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              >
-                Sign in
-              </button>
+              <Button />
             </div>
           </div>
         </a>
