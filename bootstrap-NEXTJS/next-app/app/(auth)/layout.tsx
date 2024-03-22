2c0b25969c90c9e1c5e08300e3f169a7180a36ec
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-22 12:38:03 +0530

    added and ignored the add folder

--- bootstrap-NEXTJS/next-app/app/(auth)/layout.tsx
+++ bootstrap-NEXTJS/next-app/app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 export default function ({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <div className="border-b p-1">20% off for the next few days!</div>
+      <div className="border-b p-1 text-center">
+        20% off for the next few days!
+      </div>
       {children}
     </div>
   );
