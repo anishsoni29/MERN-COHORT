24d8de9ee66532cb5d06bf881eb5d36f9299b1c9
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-22 12:59:28 +0530

    added "use client " on use state pages --> made it a client components from server component (imp)

--- bootstrap-NEXTJS/next-app/components/Signin.tsx
+++ bootstrap-NEXTJS/next-app/components/Signin.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 export function Signin() {
+  function handler() {
+    console.log("sign in button clicker");
+  }
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -18,6 +23,7 @@ export function Signin() {
                 placeholder="123456"
               />
               <button
+                onClick={handler}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
