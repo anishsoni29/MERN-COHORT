894e063525f1c729652131823a918d88c7ae4f2b
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-23 12:33:37 +0530

    changed the docker file

--- app-tw/app/api/user/route.ts
+++ app-tw/app/api/user/route.ts
@@ -2,6 +2,8 @@ import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
+
+  //posting this data to the database
   console.log(body);
   return Response.json({
     message: " Your are now logged in!",
--- app-tw/components/Signup.tsx
+++ app-tw/components/Signup.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
 
@@ -35,11 +34,14 @@ export function Signup() {
                 placeholder="123456"
               />
               <button
-                onClick={() => {
-                  axios.post("http://localhost:3000/api/user", {
-                    username,
-                    password,
-                  });
+                onClick={async () => {
+                  const response = axios.post(
+                    "http://localhost:3000/api/user",
+                    {
+                      username,
+                      password,
+                    }
+                  );
                 }}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
