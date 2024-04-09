2931ec37e608e2d56bd7e60870de5cc547862071
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-09 14:22:54 +0530

    added socket variables

--- week-19-websockets/projects/react-ws/src/App.tsx
+++ week-19-websockets/projects/react-ws/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<any>(null);
+  const [latestMessage, setLatestMessage] = useState("");
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log("Connected to server");
+      setSocket(socket);
+    };
+    socket.onmessage = (message) => {
+      console.log("Message received: ", message.data);
+      setLatestMessage(message.data);
+    };
+    setSocket(socket);
+  }, []);
+
+  if (!socket) {
+    return <div>Connecting to the Socket Server!</div>;
+  }
+  //while the socket connection is being generated, the user sees Loading..
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input></input>
+      <button
+        onClick={() => {
+          socket.send("Hello World!");
+        }}
+      >
+        Send
+      </button>
+      {latestMessage}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
