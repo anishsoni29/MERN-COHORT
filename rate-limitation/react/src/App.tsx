15cd55e0da212f615dee61d4b5d1067be3697fb2
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-27 22:55:03 +0530

    implemented the front end react changes

--- rate-limitation/react/src/App.tsx
+++ rate-limitation/react/src/App.tsx
@@ -8,16 +8,12 @@ import "./App.css";
 function App() {
   const [token, setToken] = useState<string>("");
 
-  const [otp, setOtp] = useState<string>("");
-  const [newPassword, setNewPassword] = useState<string>("");
-
   const handleUpdatePassword = () => {
     axios
       .post("http://localhost:3000/reset-password", {
         email: "sonianish441@gmail.com",
-        otp: otp,
-        token: token,
-        newPassword: newPassword,
+        otp: "212334",
+        token: "",
       })
       .then((response) => {
         console.log(response.data); // Handle success response
@@ -29,18 +25,8 @@ function App() {
 
   return (
     <>
-      <input
-        type="text"
-        placeholder="OTP"
-        value={otp}
-        onChange={(e) => setOtp(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="New Password"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
+      <input type="text" placeholder="OTP" />
+      <input type="password" placeholder="New Password" />
 
       {/* Turnstile component */}
       <Turnstile
