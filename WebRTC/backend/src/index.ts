0d08bb74a2c4d1e3a23c85f94da906c0ee79e3dc
Anish Soni <anish.21cse189@rtu.ac.in>
2024-05-24 22:31:47 +0400

    created a simple websocket server

---
+++ WebRTC/backend/src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let senderSocket: null | WebSocket = null;
+let receiverSocket: null | WebSocket = null;
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data: any) {
+    const message = JSON.parse(data);
+    console.log(message);
+  });
+
+  ws.send("something");
+});
